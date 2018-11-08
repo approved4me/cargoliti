437406e5ca530b0ad6f3e10058e4024937975805
approved4me <approved4me@gmail.com>
2018-11-08 07:21:03 +0100

    ...

--- lcl.php
+++ lcl.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
 session_start();
 require_once("inc/config.inc.php");
 require_once("inc/functions.inc.php");
 include("templates/header.inc.php");
 ?>
 
-  
+
  <div class="container">
-	<h1>Willkommen</h1>
+	<?php
+	 require_once("inc/lcl_erfassung.inc.php");
+	?>
 </div>
-  
-<?php 
-include("templates/footer.inc.php")
+
+<?php
+include("../templates/footer.inc.php")
 ?>
