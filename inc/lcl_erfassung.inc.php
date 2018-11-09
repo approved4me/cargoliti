3ec71423c3813af44c6238874490098cc40a2d32
approved4me <approved4me@gmail.com>
2018-11-09 07:31:16 +0100

    ...

--- inc/lcl_erfassung.inc.php
+++ inc/lcl_erfassung.inc.php
@@ -5,9 +5,14 @@ if	(isset($_POST['erfassen'])) {
 
 	$user = $_POST["user"];
 	$ref_lager = $_POST["ref_lager"];
+	$ref_kunde = $_POST["ref_kunde"];
+	$mail_kunde =$_POST["mail_kunde"];
+	$ref_agent = $_POST["ref_agent"];
+	$mail_agent =$_POST["mail_agent"];
+
 
 	echo $user;
-	echo $ref_lager;
+	echo $ref_agent;
 
 }
 ?>
